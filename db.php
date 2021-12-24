8a40e8f4cae49ef9a36d246823b5ed562326f561
LilDanil228 <95535003+LilDanil228@users.noreply.github.com>
2021-12-25 01:42:55 +0300

    Update db.php

--- db.php
+++ db.php
@@ -5,6 +5,7 @@ $con = mysqli_connect("localhost","root","226001080729","register");
 // Check connection
 if (mysqli_connect_errno())
   {
+  
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 ?>
